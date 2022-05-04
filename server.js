418f07844955f2c48f4db2c8775f5c11a488bfd8
MarioMog <mamg3108@gmail.com>
2022-05-03 21:07:25 -0500

    feat: endpoint DELETE certain explorer2's

--- server.js
+++ server.js
@@ -95,6 +95,12 @@ app.put('/explorers2/:id', async (req, res) => {
   return res.json({ message: 'Actualizado correctamente' })
 })
 
+app.delete('/explorers2/:id', async (req, res) => {
+  const id = parseInt(req.params.id)
+  await prisma.explorer2.delete({ where: { id: id } })
+  return res.json({ message: 'Eliminado correctamente' })
+})
+
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`)
 })
